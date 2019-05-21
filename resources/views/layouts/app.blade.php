178844eeb6d5e1e477a31eb4ce8c871f3e561fc7
zaneklavina <zaneklava@gmail.com>
2019-05-21 22:08:04 +0300

    @charset UTF-8
    UTF-8 kļūdas labošana

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
+        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{asset('css/app.css')}}">
         <title>{{config('app.name', 'Garantijas')}}</title>
