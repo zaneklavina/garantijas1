29bd8c0eb662437b51b993e1406c049367051e96
zaneklavina <zaneklava@gmail.com>
2019-05-21 13:05:55 +0300

    title to id
    changes in text

--- database/migrations/2019_05_19_191046_create_projects_table.php
+++ database/migrations/2019_05_19_191046_create_projects_table.php
@@ -14,7 +14,7 @@ class CreateProjectsTable extends Migration
     public function up()
     {
         Schema::create('projects', function (Blueprint $table) {
-            $table->string('id');
+            $table->string('id',8)->unique()->primary('id');
             $table->string('title');
             $table->mediumText('body');
             $table->timestamps();
